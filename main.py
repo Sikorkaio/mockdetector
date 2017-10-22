176a155cd3b950385cc49f75e194ef17a757241b
Lefteris Karapetsas <lefteris@refu.co>
2017-10-22 12:08:46 +0200

    Super simple bluetooth RFCOM server

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import click
-import bluetooth
+from bluetooth import (
+    BluetoothSocket,
+    RFCOMM,
+    PORT_ANY,
+    SERIAL_PORT_CLASS,
+    SERIAL_PORT_PROFILE,
+    advertise_service,
+    discover_devices,
+    lookup_name,
+)
 
 
 def connect_to_phone(target_name):
     target_address = None
 
-    nearby_devices = bluetooth.discover_devices()
+    nearby_devices = discover_devices()
     for bdaddr in nearby_devices:
-        if target_name == bluetooth.lookup_name(bdaddr):
+        if target_name == lookup_name(bdaddr):
             target_address = bdaddr
             break
 
@@ -17,15 +26,61 @@ def connect_to_phone(target_name):
         print ("could not find target bluetooth device nearby")
 
 
+def run_bt_server():
+    """Adapted from: https://github.com/EnableTech/raspberry-bluetooth-demo"""
+    server_sock = BluetoothSocket(RFCOMM)
+
+    server_sock.bind(("", PORT_ANY))
+    server_sock.listen(1)
+
+    port = server_sock.getsockname()[1]
+    print ("listening on port %d" % port)
+
+    uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
+    advertise_service(
+        server_sock,
+        "Mock Detector",
+        service_id=uuid,
+        service_classes=[uuid, SERIAL_PORT_CLASS],
+        profiles=[SERIAL_PORT_PROFILE],
+        # protocols=[OBEX_UUID],
+    )
+
+    print("Waiting for connection on RFCOMM channel %d" % port)
+
+    client_sock, client_info = server_sock.accept()
+    print("Accepted connection from ", client_info)
+
+    try:
+        while True:
+            data = client_sock.recv(1024)
+            if len(data) == 0:
+                break
+            print("received [%s]" % data)
+            if data == b'g':
+                client_sock.send("YO THIS THE DETECTOR")
+    except IOError:
+        pass
+
+    print("disconnected")
+
+    client_sock.close()
+    server_sock.close()
+    print("all done")
+
+
 @click.option(
     '--devicename',
-    required=True,
+    required=False,
     help='Name of the bluetooth device to connect to'
 )
 @click.group(invoke_without_command=True)
 @click.pass_context
 def main(ctx, **kwargs):
-    connect_to_phone(kwargs['devicename'])
+    if 'devicename' in kwargs and kwargs['devicename'] is not None:
+        connect_to_phone(kwargs['devicename'])
+    else:
+        run_bt_server()
 
 
 if __name__ == "__main__":
